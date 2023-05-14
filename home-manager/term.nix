ddd9d088c067dcd002816befc0256ab9ff0a6d46
Alan Hettinger <alan.hettinger@proton.me>
2023-05-14 03:36:38 -0400

    Added starship config

--- home-manager/term.nix
+++ home-manager/term.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }: {
+{ config, lib, pkgs, ... }: {
 
   home.packages = with pkgs; [
     tmux
@@ -39,6 +39,122 @@
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      palette = "catppuccin_macchiato";
+      add_newline = false;
+      format = lib.concatStrings [
+        "$directory"
+        "$nix_shell"
+        "$git_branch"
+        "$git_state"
+        "$git_status"
+        "$git_metrics"
+        "$fill"
+        "$cmd_duration"
+        "$jobs"
+        "$sudo"
+        "$time"
+        "$line_break"
+        "$character"
+      ];
+      directory = {
+        style = "blue";
+        read_only = "  "; # # formatting looks wrong but this is a lock icon
+      };
+      character = {
+        success_symbol = "[->](green)";
+        error_symbol = "[X->](red)";
+        vimcmd_symbol = "[<](green)";
+      };
+      git_branch = {
+        format = "[$branch]($style)";
+        style = "yellow";
+        symbol = " ";
+      };
+      git_status = {
+        format =
+          "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+        style = "green";
+        conflicted = "​";
+        untracked = "​";
+        modified = "​";
+        staged = "​";
+        renamed = "​";
+        deleted = "​";
+        stashed = "≡";
+      };
+      git_state = {
+        format = "([$state( $progress_current/$progress_total)]($style)) ";
+        style = "blue";
+      };
+      git_metrics = {
+        disabled = false;
+        format = "([+$added]($added_style) )([-$deleted]($deleted_style) )";
+        added_style = "sky";
+        deleted_style = "peach";
+      };
+      cmd_duration = {
+        format = "[$duration]($style) ";
+        style = "lavender";
+      };
+      fill = {
+        symbol = "·";
+        style = "subtext0";
+      };
+      time = {
+        disabled = false;
+        use_12hr = true;
+        style = "rosewater";
+        format = " [$time]($style) ";
+      };
+      nix_shell = {
+        disabled = false;
+        format = "[$symbol $state $symbol  ]($style)";
+        style = "lavender";
+        symbol = "";
+        impure_msg = "[ impure nix shell](blue)";
+        pure_msg = "[ pure nix shell](green)";
+        unknown_msg = "[ nix shell](yellow)";
+        heuristic = true;
+      };
+      sudo = {
+        disabled = false;
+        symbol = "\\(#\\) ";
+        style = "mauve";
+        format = "[$symbol]($style)";
+      };
+
+      palettes = {
+        catppuccin_macchiato = {
+          rosewater = "#f4dbd6";
+          flamingo = "#f0c6c6";
+          pink = "#f5bde6";
+          mauve = "#c6a0f6";
+          red = "#ed8796";
+          maroon = "#ee99a0";
+          peach = "#f5a97f";
+          yellow = "#eed49f";
+          green = "#a6da95";
+          teal = "#8bd5ca";
+          sky = "#91d7e3";
+          sapphire = "#7dc4e4";
+          blue = "#8aadf4";
+          lavender = "#b7bdf8";
+          text = "#cad3f5";
+          subtext1 = "#b8c0e0";
+          subtext0 = "#a5adcb";
+          overlay2 = "#939ab7";
+          overlay1 = "#8087a2";
+          overlay0 = "#6e738d";
+          surface2 = "#5b6078";
+          surface1 = "#494d64";
+          surface0 = "#363a4f";
+          base = "#24273a";
+          mantle = "#1e2030";
+          crust = "#181926";
+        };
+      };
+    };
   };
 
   programs.alacritty = {
