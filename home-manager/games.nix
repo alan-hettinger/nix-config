200b8ab52dbd56d0de97918c2f6d71a03fb826ae
Alan <alan.hettinger@proton.me>
2023-06-03 01:59:13 -0400

    added steam and ffxiv - tested and working

--- home-manager/games.nix
+++ home-manager/games.nix
@@ -1,18 +1,15 @@
 { config, lib, pkgs, ... }: {
-  home.packages = with pkgs;
-    [
-      (retroarch.override {
-        cores = with libretro; [
-          bsnes
-          mupen64plus
-          nestopia
-          mgba
-          steam
-          xivlauncher
+  home.packages = with pkgs; [
+    (retroarch.override {
+      cores = with libretro; [
+        bsnes
+        mupen64plus
+        nestopia
+        mgba
 
-        ];
-      })
-
-    ];
-
+      ];
+    })
+    steam
+    xivlauncher
+  ];
 }
