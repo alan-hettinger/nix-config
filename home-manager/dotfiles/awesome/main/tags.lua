3d96aea31bf9ce12da9c996a4736e9a0b7e894df
Alan <alan.hettinger@proton.me>
2023-08-27 18:29:07 -0400

    refactor awesomewm tags file

--- home-manager/dotfiles/awesome/main/tags.lua
+++ home-manager/dotfiles/awesome/main/tags.lua
@@ -9,264 +9,100 @@ local lain       = require("lain")
 
 local theme      = require("theme")
 local iconsdir   = os.getenv("HOME") .. "/.config/awesome/assets/icons/"
-
-local screen2    = RC.vars.screen2_maybe
-
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
---
 
 function _M.get()
-  -- local tags = {}
-  -- local tagpairs = {
-  --   names = { "1:  ", "2:  ", "3:  ", "4:  ", "5:  ", "6:  ", "7:  ", "8:  ", "9:  ", },
-  --   --  
-  --   layout = {
-  --     RC.layouts[1], RC.layouts[1], RC.layouts[1], RC.layouts[1], RC.layouts[1], RC.layouts[2], RC.layouts[1],
-  --     RC.layouts[1], RC.layouts[1],
-  --   },
-  --   master_width_factor = 0.6,
-  -- }
-  -- awful.screen.connect_for_each_screen(function(s)
-  --   tags[s] = awful.tag(tagpairs.names, s, tagpairs.layout)
-  -- end)
-  -- return tags
   local tags = {}
-  local screen1_tags = {
-    awful.tag.add("1:  ", {
-      -- icon               = "",
-      index              = 1,
+
+  local screen1 = {
+    tag_names = { "1:  ", "2:  ", "3:  ", "4:  ", "5:  ", "6:  ", "7:  ", "8:  ", "9:  " },
+    defaults = {
       layout             = awful.layout.suit.tile,
-      -- layouts = [] -- table of layouts available
-      master_fill_policy = theme.master_fill_policy,
+      master_fill_policy = "expand", -- "master_width_factor"|"expand"
       master_count       = theme.master_count,
       gap_single_client  = theme.gap_single_client,
       gap                = theme.useless_gap,
       column_count       = theme.column_count,
-      -- screen             = s,
-      selected           = true,
-    }),
-
-    awful.tag.add("2:  ", {
-      -- icon               = "",
-      index              = 2,
-      layout             = awful.layout.suit.tile,
-      -- layouts = [] -- table of layouts available
-      master_fill_policy = theme.master_fill_policy,
-      master_count       = theme.master_count,
-      gap_single_client  = theme.gap_single_client,
-      gap                = theme.useless_gap,
-      column_count       = theme.column_count,
-      -- screen             = s,
-    }),
-
-    awful.tag.add("3:  ", {
-      -- icon               = "",
-      index              = 3,
-      layout             = awful.layout.suit.tile,
-      -- layouts = [] -- table of layouts available
-      master_fill_policy = theme.master_fill_policy,
-      master_count       = theme.master_count,
-      gap_single_client  = theme.gap_single_client,
-      gap                = theme.useless_gap,
-      column_count       = theme.column_count,
-      -- screen             = s,
-    }),
-
-    awful.tag.add("4:  ", {
-      -- icon               = "",
-      index              = 4,
-      layout             = awful.layout.suit.tile,
-      -- layouts = [] -- table of layouts available
-      master_fill_policy = theme.master_fill_policy,
-      master_count       = theme.master_count,
-      gap_single_client  = theme.gap_single_client,
-      gap                = theme.useless_gap,
-      column_count       = theme.column_count,
-      -- screen             = s,
-    }),
-
-    awful.tag.add("5:  ", {
-      -- icon               = "",
-      index              = 5,
-      layout             = awful.layout.suit.magnifier,
-      -- layouts = [] -- table of layouts available
-      master_fill_policy = 0.8,
-      master_count       = theme.master_count,
-      gap_single_client  = theme.gap_single_client,
-      gap                = theme.useless_gap,
-      column_count       = theme.column_count,
-      -- screen             = s,
-    }),
-
-    awful.tag.add("6:  ", {
-      -- icon               = "",
-      index              = 6,
-      layout             = lain.layout.termfair.center,
-      -- layouts = [] -- table of layouts available
-      master_fill_policy = theme.master_fill_policy,
-      master_count       = theme.master_count,
-      gap_single_client  = theme.gap_single_client,
-      gap                = dpi(25),
-      column_count       = theme.column_count,
-      -- screen             = s,
-    }),
-
-    awful.tag.add("7:  ", {
-      -- icon               = "",
-      index              = 7,
-      layout             = awful.layout.suit.tile,
-      -- layouts = [] -- table of layouts available
-      master_fill_policy = theme.master_fill_policy,
-      master_count       = theme.master_count,
-      gap_single_client  = theme.gap_single_client,
-      gap                = theme.useless_gap,
-      column_count       = theme.column_count,
-      -- screen             = s,
-    }),
-
-    awful.tag.add("8:  ", {
-      -- icon               = "",
-      index              = 8,
-      layout             = awful.layout.suit.tile,
-      -- layouts = [] -- table of layouts available
-      master_fill_policy = theme.master_fill_policy,
-      master_count       = theme.master_count,
-      gap_single_client  = theme.gap_single_client,
-      gap                = theme.useless_gap,
-      column_count       = theme.column_count,
-      -- screen             = s,
-    }),
-
-    awful.tag.add("9:  ", {
-      -- icon               = "",
-      index              = 9,
-      layout             = awful.layout.suit.tile,
-      -- layouts = [] -- table of layouts available
-      master_fill_policy = theme.master_fill_policy,
-      master_count       = theme.master_count,
-      gap_single_client  = theme.gap_single_client,
-      gap                = theme.useless_gap,
-      column_count       = theme.column_count,
-      -- screen             = s,
-    }),
+      screen             = RC.vars.screen1,
+    },
+    default_selected_tag = 1,
+    overrides = {
+      -- Overrides should always be of the form { tag index, property, value }
+      { 5, "layout",             awful.layout.suit.magnifier },
+      { 5, "master_fill_policy", 0.8 },
+      { 6, "layout",             lain.layout.termfair.center },
+      { 6, "gap",                dpi(25) },
+    }
   }
 
-  local mon2_fill_policy = theme.master_fill_policy -- "master_width_factor"
-
-  -- tags for the second screen:
-  local screen2_tags = {
-    awful.tag.add("1:  ", {
-      -- icon               = "",
-      index              = 1,
+  local screen2 = {
+    tag_names = { "1:  ", "2:  ", "3:  ", "4:  ", "5:  ", "6:  ", "7:  ", "8:  " },
+    defaults = {
       layout             = awful.layout.suit.tile.bottom,
-      -- layouts = [] -- table of layouts available
-      master_fill_policy = mon2_fill_policy,
+      master_fill_policy = "expand", -- "master_width_factor"|"expand"
       master_count       = theme.master_count,
       gap_single_client  = theme.gap_single_client,
       gap                = theme.useless_gap,
       column_count       = theme.column_count,
-      screen             = screen2,
-      selected           = true,
-    }),
-
-    awful.tag.add("2:  ", {
-      -- icon               = "",
-      index              = 2,
-      layout             = awful.layout.suit.tile.bottom,
-      -- layouts = [] -- table of layouts available
-      master_fill_policy = mon2_fill_policy,
-      master_count       = theme.master_count,
-      gap_single_client  = theme.gap_single_client,
-      gap                = theme.useless_gap,
-      column_count       = theme.column_count,
-      screen             = screen2,
-    }),
-
-    awful.tag.add("3:  ", {
-      -- icon               = "",
-      index              = 3,
-      layout             = awful.layout.suit.tile.bottom,
-      -- layouts = [] -- table of layouts available
-      master_fill_policy = mon2_fill_policy,
-      master_count       = theme.master_count,
-      gap_single_client  = theme.gap_single_client,
-      gap                = theme.useless_gap,
-      column_count       = theme.column_count,
-      screen             = screen2,
-    }),
-
-    awful.tag.add("4:  ", {
-      -- icon               = "",
-      index              = 4,
-      layout             = awful.layout.suit.tile.bottom,
-      -- layouts = [] -- table of layouts available
-      master_fill_policy = mon2_fill_policy,
-      master_count       = theme.master_count,
-      gap_single_client  = theme.gap_single_client,
-      gap                = theme.useless_gap,
-      column_count       = theme.column_count,
-      screen             = screen2,
-    }),
-
-    awful.tag.add("5:  ", {
-      -- icon               = "",
-      index              = 5,
-      layout             = awful.layout.suit.tile.bottom,
-      -- layouts = [] -- table of layouts available
-      master_fill_policy = mon2_fill_policy,
-      master_count       = theme.master_count,
-      gap_single_client  = theme.gap_single_client,
-      gap                = theme.useless_gap,
-      column_count       = theme.column_count,
-      screen             = screen2,
-    }),
-
-    awful.tag.add("6:  ", {
-      -- icon               = "",
-      index              = 6,
-      layout             = awful.layout.suit.tile.bottom,
-      -- layouts = [] -- table of layouts available
-      master_fill_policy = mon2_fill_policy,
-      master_count       = theme.master_count,
-      gap_single_client  = theme.gap_single_client,
-      gap                = theme.useless_gap,
-      column_count       = theme.column_count,
-      screen             = screen2,
-    }),
-
-    awful.tag.add("7:  ", {
-      -- icon               = "",
-      index              = 7,
-      layout             = awful.layout.suit.tile.bottom,
-      -- layouts = [] -- table of layouts available
-      master_fill_policy = mon2_fill_policy,
-      master_count       = theme.master_count,
-      gap_single_client  = theme.gap_single_client,
-      gap                = theme.useless_gap,
-      column_count       = theme.column_count,
-      screen             = screen2,
-    }),
-
-    awful.tag.add("8:  ", {
-      -- icon               = "",
-      index              = 8,
-      layout             = awful.layout.suit.tile.bottom,
-      -- layouts = [] -- table of layouts available
-      master_fill_policy = mon2_fill_policy,
-      master_count       = theme.master_count,
-      gap_single_client  = theme.gap_single_client,
-      gap                = theme.useless_gap,
-      column_count       = theme.column_count,
-      screen             = screen2,
-    }),
+      screen             = RC.vars.screen2_maybe,
+    },
+    default_selected_tag = 6,
   }
+
+  local setup_per_screen = function(target_screen)
+    local make_tag_table = function(tag_names, target_table)
+      for I = 1, #tag_names do
+        target_table[I] = awful.tag.add(tag_names[I], { index = I })
+      end
+    end
+
+    local set_defaults = function()
+      local props = { 'layout', 'master_fill_policy', 'master_count', 'gap_single_client', 'gap', 'column_count',
+        'screen' }
+      for I = 1, #target_screen.tags do
+        local thistag = target_screen.tags[I]
+        for v = 1, #props do
+          local thisprop = props[v]
+          -- if thistag[v] == nil then
+          thistag[thisprop] = target_screen.defaults[thisprop]
+          -- end
+        end
+      end
+      target_screen.tags[target_screen.default_selected_tag].selected = true
+    end
+
+
+    local override = function(tag, prop, val)
+      target_screen.tags[tag][prop] = val
+    end
+    local set_overrides = function()
+      if target_screen.overrides ~= nil then
+        for I = 1, #target_screen.overrides do
+          local tag = target_screen.overrides[I][1]
+          local prop = target_screen.overrides[I][2]
+          local val = target_screen.overrides[I][3]
+          override(tag, prop, val)
+        end
+      end
+    end
+
+    target_screen.tags = {}
+    make_tag_table(target_screen.tag_names, target_screen.tags)
+    set_defaults()
+    set_overrides()
+  end
+
+  setup_per_screen(screen1)
+  setup_per_screen(screen2)
+
   -- set up second monitor:
+  local my_tags = {}
   if screen.count == 1 then
-    my_tags = screen1_tags
+    my_tags = screen1.tags
   else
-    my_tags = { unpack(screen1_tags) }
-    for I = 1, #screen2_tags do
-      my_tags[#screen1_tags + I] = screen2_tags[I]
+    my_tags = { table.unpack(screen1.tags) }
+    for I = 1, #screen2.tags do
+      my_tags[#screen1.tags + I] = screen2.tags[I]
     end
   end
 
