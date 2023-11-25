4350643e17986cea5ae69170b2445b84d742fe5e
Alan <alan.hettinger@proton.me>
2023-11-24 23:08:28 -0500

    add function to generate system outputs

--- flake.nix
+++ flake.nix
@@ -26,59 +26,47 @@
         # credit to https://github.com/kclejeune/system/blob/2ae7ced193f862ae3deace320c37f4657a873bd0/flake.nix#L49
         nixpkgs.lib.extend
         (final: prev: (import ./lib final) // home-manager.lib);
-
       lib = (mkLib nixpkgs);
 
-    in {
-      # Available through 'sudo nixos-rebuild --flake .#your-hostname'
-      nixosConfigurations = {
+      generateNixosSystem = { system ? "x86_64-linux"
+        , specialArgs ? { inherit inputs lib; }, systemModule, games ? false
+        , hardwareModules ? [ ], desktopModules ? [
+          ./desktop/xorg.nix
+          ./appearance
+          stylix.nixosModules.stylix
+        ], hmModules ? [
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useUserPackages = true;
+            home-manager.users.alan = import ./home-manager/home.nix;
+          }
+        ], extraModules ? [ ], }:
+        nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          inherit system;
+          modules = systemModule ++ hardwareModules ++ desktopModules
+            ++ extraModules
+            ++ (if games == true then [ ./software/games.nix ] else [ ])
+            ++ [ ./common/common.nix ] ++ hmModules;
+        };
 
-        alan-desktop-linux = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs lib; };
-          modules = [
-            ./desktop/xorg.nix
-            ./systems/desktop
-            ./software/games.nix
-            ./appearance
-            ./common/common.nix
-            # ./helper-functions.nix
-            stylix.nixosModules.stylix
+    in {
+      nixosConfigurations = {
+        alan-desktop-linux = generateNixosSystem {
+          systemModule = [ ./systems/desktop ];
+          games = true;
+          hardwareModules = [
             nixos-hardware.nixosModules.common-cpu-amd
             nixos-hardware.nixosModules.common-pc-ssd
             nixos-hardware.nixosModules.common-gpu-amd
-
-            home-manager.nixosModules.home-manager
-            {
-              # home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.alan = import ./home-manager/home.nix;
-            }
-
-            # hyprland.nixosModules.default
-            # hyprland.homeManagerModules.default
           ];
         };
 
-        alan-laptop-linux = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./desktop/xorg.nix
-            ./systems/laptop
-            ./appearance
-            ./common/common.nix
-            stylix.nixosModules.stylix
-            nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen2
-
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.alan = import ./home-manager/home.nix;
-            }
-
-            # hyprland.nixosModules.default
-            # hyprland.homeManagerModules.default
-          ];
+        alan-laptop-linux = generateNixosSystem {
+          systemModule = [ ./systems/laptop ];
+          games = true;
+          hardwareModules =
+            [ nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen2 ];
         };
       };
     };
