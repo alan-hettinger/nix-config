a2a30408231f7f76c130aaf5da6951af8822de5e
Alan <alan.hettinger@proton.me>
2024-01-04 20:29:28 -0500

    reformat

--- home-manager/dotfiles/doom.d/writing/thesis.el
+++ home-manager/dotfiles/doom.d/writing/thesis.el
@@ -1,25 +1,22 @@
 ;;; thesis.el -*- lexical-binding: t; -*-
-
 ;; configuration that is specifically useful for my MA thesis, written in org-mode
 (after! org
 
-  ;; automatically export my MA thesis to pdf on save:
-  (let* ((main-filename-str "thesis-draft")
-         (date-format-str (format-time-string "%Y-%m-%d"))
-         (target-path "./drafts/")
-         (target-filename (format "%s%s-%s.tex" target-path date-format-str main-filename-str)))
-    (defun alan/export-thesis-draft ()
+  ;; automatically export thesis to pdf on save:
+  (defun alan/export-thesis-draft ()
+    (let* ((main-filename-str "thesis-draft")
+           (date-format-str (format-time-string "%Y-%m-%d"))
+           (target-path "./drafts/")
+           (target-filename (format "%s%s-%s.tex" target-path date-format-str main-filename-str)))
       (when (string-equal (buffer-file-name) (expand-file-name "./thesis-draft.org"))
         (progn
           ;; create the pdf:
           (org-export-to-file 'latex target-filename
-            nil             ;; export asynchronously
-            nil nil nil nil ;; bunch of args I don't care about
+            nil              ;; export asynchronously
+            nil nil nil nil  ;; bunch of args I don't care about
             #'org-latex-compile)
           ;; clean up the latex file:
-          (delete-file target-filename))
-        )
-      ))
+          (delete-file target-filename)))))
   (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'alan/export-thesis-draft)))
 
   ;; handle the bibliography:
