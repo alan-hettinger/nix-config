72eacc1d5f325d6944ba104913739736d2c56511
Alan <alan.hettinger@proton.me>
2024-03-07 19:16:03 -0500

    make vpn work

--- systems/desktop/network.nix
+++ systems/desktop/network.nix
@@ -3,13 +3,29 @@
   networking = {
     hostName = "alan-desktop-linux";
 
+    networkmanager = {
+      enable = true;
+      dns = "default";
+      extraConfig = ''
+        [main]
+        rc-manager=symlink
+      '';
+    };
+
+    wireguard.enable = true;
+    iproute2.enable = true;
+    resolvconf.enable = false;
+
     firewall = {
-      enable = false;
+      enable = true;
       allowedTCPPorts = [
         22 # ssh
+        53 # needed by mullvad
         80 # web
         443 # web
+        1401 # needed by Mullvad
       ];
+      allowedUDPPorts = [ 51820 53 1194 1195 1197 1300 1301 1302 1303 1400 ];
     };
   };
 
