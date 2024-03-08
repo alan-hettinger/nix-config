727b69d52d65ecdf26da247ed689bb06d44f6c51
Alan <alan.hettinger@proton.me>
2024-03-07 22:08:10 -0500

    fill out fs mount points for extra drives

--- systems/desktop/default.nix
+++ systems/desktop/default.nix
@@ -16,15 +16,29 @@
 
   ## TODO split this into a separate file
   fileSystems = {
-    "/media" = {
-      device = "/dev/sdb1";
+    "/mnt/media" = {
+      device = "/dev/disk/by-uuid/3bee2601-fc59-43ee-82e2-02f8e0abc421";
       fsType = "ext4";
-      options = [
-        "users"
-        "nofail"
-        "defaults"
+      options = [ "users" "nofail" "defaults" "x-gvfs-show" ];
+    };
+    "/mnt/extra-ssd" = {
+      device = "/dev/disk/by-uuid/ea13dce9-1054-476a-8394-e34a276d5bae";
+      fsType = "ext4";
+      options = [ "users" "nofail" "defaults" "x-gvfs-show" ];
+    };
 
-      ];
+    ## bind-mounting the above partitions into /home/alan/...
+    "/home/alan/media" = {
+      device = "/mnt/media";
+      fsType = "none";
+      depends = [ "/mnt/media" ];
+      options = [ "defaults" "bind" ];
+    };
+    "/home/alan/extra-ssd" = {
+      device = "/mnt/extra-ssd";
+      fsType = "none";
+      depends = [ "/mnt/extra-ssd" ];
+      options = [ "defaults" "bind" ];
     };
   };
 }
