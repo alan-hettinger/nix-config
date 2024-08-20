a9f6e91d35c984bde5f3808b2b98a0854f053e44
Alan <alan.hettinger@proton.me>
2024-08-19 22:55:06 -0400

    waybar: add battery for laptop

--- systems/hm-laptop/wl-output.nix
+++ systems/hm-laptop/wl-output.nix
@@ -7,4 +7,11 @@
   wayland.windowManager.hyprland.settings = {
     monitor = ["eDP-1, 1920x1080, auto, 1"];
   };
+  programs.waybar = {
+    settings.mainBar = {
+      modules-right = [
+        "battery"
+      ];
+    };
+  };
 }
