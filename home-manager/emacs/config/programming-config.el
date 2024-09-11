014adf018620462eee07e732ee09049f6fb6a905
Alan <alan.hettinger@proton.me>
2024-09-11 15:05:03 -0400

    emacs: use lispy(ville) for elisp

--- home-manager/emacs/config/programming-config.el
+++ home-manager/emacs/config/programming-config.el
@@ -5,8 +5,11 @@
 (add-hook 'highlight-indent-guides-mode-hook
           (lambda () (setq highlight-indent-guides-method 'bitmap
                            highlight-indent-guides-responsive 'top)))
-(add-hook 'prog-mode-hook #'highlight-indent-guides-mode)
+;; (add-hook 'prog-mode-hook #'highlight-indent-guides-mode) ;; FIXME this causes some kind of error with completion
 
+(add-hook 'lispy-mode-hook #'lispyville-mode)
+(add-hook 'emacs-lisp-mode-hook #'lispy-mode)
+(add-hook 'lisp-mode-hook #'lispy-mode)
 ;; HACK flycheck doesn't recognize elisp packages installed by nix,
 ;; which causes erroneous undefined variable warnings constantly.
 (add-hook 'emacs-lisp-mode-hook (lambda () (flycheck-mode -1)))
