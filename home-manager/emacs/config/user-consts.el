a6dc63e5e82bd01f4dddb3684c01881329046cc5
Alan <alan.hettinger@proton.me>
2024-09-13 02:14:51 -0400

    emacs: change font to Iosevka, mononoki doesn't have multiple weights

--- home-manager/emacs/config/user-consts.el
+++ home-manager/emacs/config/user-consts.el
@@ -8,7 +8,7 @@
 (defconst alan/disable-all-scrollbars t
   "Whether to disable all scroll bars, or just superfluous ones.")
 
-(defconst alan/mono-font "mononoki"
+(defconst alan/mono-font "Iosevka Nerd Font"
   "The mono-width font to use.")
 (defconst alan/serif-font "Source Serif Variable")
 (defconst alan/sans-font "Source Sans Variable")
