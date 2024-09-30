02c2a33c1003a65b062424a24c820c6f9981c02d
Alan <alan.hettinger@proton.me>
2024-09-30 14:01:51 -0400

    nix: enable direnv

--- home-manager/dev.nix
+++ home-manager/dev.nix
@@ -7,4 +7,12 @@
     gnumake
     github-desktop
   ];
+  programs = {
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+      nix-direnv.enable = true;
+    };
+  };
 }
