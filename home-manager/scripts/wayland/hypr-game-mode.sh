b7f2edc00b4fc3177bcff8d30699c1c08e257370
Alan <alan.hettinger@proton.me>
2024-11-19 14:22:17 -0500

    hyprland: follow renamed settings

--- home-manager/scripts/wayland/hypr-game-mode.sh
+++ home-manager/scripts/wayland/hypr-game-mode.sh
@@ -4,7 +4,7 @@ HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
 if [ "$HYPRGAMEMODE" = 1 ]; then
     hyprctl --batch "\
     keyword animations:enabled 0;\
-    keyword decoration:drop_shadow 0;\
+    keyword decoration:shadow:enabled 0;\
     keyword decoration:blur:enabled 0;\
     keyword decoration:rounding 0"
     systemctl --user stop wlsunset.service
