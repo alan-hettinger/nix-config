a17e72eb45fc4e64bd520e982a5bd21e0951ca33
Alan <alan.hettinger@proton.me>
2025-01-14 20:43:36 -0500

    Update flake; fix local pkg "roll", build failure due to racket update
    Package "roll" threw build error after racket update. Changed racket vm
    implementation. See comment in pkgs/roll/default.nix.

--- pkgs/roll/default.nix
+++ pkgs/roll/default.nix
@@ -11,11 +11,12 @@ stdenv.mkDerivation rec {
 
   src = ./.;
 
-  nativeBuildInputs = [];
+  nativeBuildInputs = [pkgs.racket];
   buildInputs = [];
 
+  ## FIXME 2025-01-14: building with --3m implementation of the racket vm rather than the default --cs implementation due to build error "find-exe: can't find Racket executable for variant cs". Likely result of Racket update 8.14 -> 8.15.
   buildPhase = ''
-    ${pkgs.racket}/bin/raco exe -o roll ./main.rkt
+    ${pkgs.racket}/bin/raco exe --3m -o roll ./main.rkt
   '';
 
   installPhase = ''
